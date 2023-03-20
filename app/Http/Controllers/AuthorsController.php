794c0e51254b0ad9ab873587b2b29f70f0cf3a74
Eben <arkohaddobenezer21@gmail.com>
2023-03-20 18:13:32 +0000

    Created Authors model, migration and controller

---
+++ app/Http/Controllers/AuthorsController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorsController extends Controller
+{
+    public function store(){
+        Author::create($this->validateRequest());
+    }
+
+
+    protected function validateRequest(){
+        return request()->validate([
+            'name'=>'required',
+            'dob'=>'required',
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use App\Http\Controllers\BooksController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::post('/books',[BooksController::class,'store']);
 Route::patch('/books/{book}',[BooksController::class,'update']);
 Route::delete('/books/{book}',[BooksController::class,'destroy']);
+
+Route::post('/authors',[AuthorsController::class,'store']);
+Route::patch('/authors/{book}',[AuthorsController::class,'update']);
+Route::delete('/authors/{book}',[AuthorsController::class,'destroy']);
+
 
 Route::get('/', function () {
     return view('welcome');

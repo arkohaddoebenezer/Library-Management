f61e859f15928fa3537a4fb2b43fc8f9149d8a04
Eben <arkohaddobenezer21@gmail.com>
2023-03-21 16:10:16 +0000

    breeze authentication

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\AuthorsController;
-use App\Http\Controllers\BooksController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -10,20 +9,23 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::post('/books',[BooksController::class,'store']);
-Route::patch('/books/{book}',[BooksController::class,'update']);
-Route::delete('/books/{book}',[BooksController::class,'destroy']);
-
-Route::post('/authors',[AuthorsController::class,'store']);
-Route::patch('/authors/{book}',[AuthorsController::class,'update']);
-Route::delete('/authors/{book}',[AuthorsController::class,'destroy']);
-
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

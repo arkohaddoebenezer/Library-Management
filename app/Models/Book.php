602553d98201fe80dbaf02c9ad8a7b5c6285c173
Eben <arkohaddobenezer21@gmail.com>
2023-03-21 10:56:26 +0000

    defined a setAtrributeAthuthorId for author

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,11 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Book extends Model
 {
     use HasFactory;
+
     public function path(){
         return "/books".$this->id;
+    }
+    public function checkout(User $user){
+
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+    public function checkin(User $user){
+
+        $reservation = $this->reservations()->where('user_id',$user->id)
+        ->whereNotNull('checked_out_at')
+        ->whereNull('checked_in_at')
+        ->first();
+
+        if(is_null($reservation)){
+            throw new \Exception();
+        }
+        
+        $reservation->update([
+            'checked_out_at'=>null
+        ]);
+    }
+
+    public function setAuthorIdAttributes($author){
+
+        $this->attributes['author_id'] = (Author::firstOrCreate([
+            'name'=>$author,
+        ]))->id;
+    }
+
+    public function reservations(){
+        return $this->hasMany(Reservation::class);
     }
 }

6582e7aa8dfadb8ef61f7b0179db5ded056c9cf7
Eben <arkohaddobenezer21@gmail.com>
2023-03-21 10:55:20 +0000

    formated dob of Author to be Carbon date

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'dob',
+    ];
+
+    protected $dates = [
+        'dob'
+    ];
+    public function setDobAttributes($dob){
+        $this->attributes['dob'] = Carbon::parse($dob);
+    }
+
+    
 }
